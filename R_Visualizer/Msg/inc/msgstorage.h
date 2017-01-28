43b9abb053cf24be1a54cbd80788d8575f9f1257
Christian Neuberger <NeubergerCh500@th-nuernberg.de>
2017-01-28 16:12:49 +0100

    Added documentation to MsgStorage; Adapted Doxyfile to new directory structure; Adapted .ycm_extra_conf to include directories;

--- R_Visualizer/Msg/inc/msgstorage.h
+++ R_Visualizer/Msg/inc/msgstorage.h
@@ -25,9 +25,17 @@ struct ContainerID{
     bool isValid() const { return ( ( ContainerNR >= 0 ) && ( IndexInStore >= 0 ) ); }
 };
 
+/**
+ * \brief Simple RingBuffer that destroys elements on overflow
+ */
 template<typename T> class RSimpleDestructiveRingBuff
 {
 public:
+    /**
+     * \brief constructs the ring buffer with the given size
+     * 
+     * \param[in] size    Size to set for the ring buffer
+     */
     RSimpleDestructiveRingBuff(const int size) :
         RBufBuffer(size),
         RBufSize(size),
@@ -37,6 +45,12 @@ public:
     {
     }
 
+    /**
+     * \brief append an element to the current end of the ring buffer
+     * 
+     * if the size of the ring buffer is exceeded the first element is overriden
+     * the ring buffers location pointers are adapted accordingly
+     */
     void append(const T &value)
     {
         if(RBufCurSize == RBufSize)
@@ -52,6 +66,11 @@ public:
         RBufBuffer.replace(RBufEndIndex, value);
     }
 
+    /** 
+     * \brief prepend an element to the current start of the ring buffer
+     * if the size of the ring buffer is exceeded the last element is overriden
+     * the ring buffers location pointers are adapted accordingly
+     */
     void prepend(const T &value)
     {
         if(RBufCurSize == RBufSize)
@@ -67,11 +86,19 @@ public:
         RBufBuffer.replace(RBufStartIndex, value);
     }
 
+    /**
+     * \brief replaces the element at the given index with the given value
+     */
     void replace(const int index, const T &value)
     {
         RBufBuffer.replace((index + RBufStartIndex ) % RBufSize, value);
     }
 
+    /**
+     * \brief removes the element at the given index from the ring buffer
+     * 
+     * The ring buffer's location pointers are adapted accordindly
+     */
     void remove(const int index)
     {
         int calcdIndex = ( index + RBufStartIndex ) % RBufSize;
@@ -104,15 +131,31 @@ public:
         }
     }
 
+    /** 
+     * \brief retrieves the current first element of the ring buffer
+     * 
+     * The first element is the element pointed to by the ring buffer's
+     * start pointer.
+     */
     const T &first() const
     {
         return RBufBuffer.at(RBufStartIndex);
     }
+
+    /**
+     * \brief retrieves the current last element of the ring buffer
+     * 
+     * The last element is the element pointed to by the ring buffer's
+     * end pointer
+     */
     const T &last() const
     {
         return RBufBuffer.at(RBufEndIndex);
     }
 
+    /**
+     * \brief retrieves the current element at the given index
+     */
     const T &at(const int index) const
     {
         const int calcdIndex = ( index + RBufStartIndex ) % RBufSize;
@@ -120,11 +163,19 @@ public:
         return RBufBuffer.at(calcdIndex);
     }
 
+    /**
+     * \brief verfifies that a given value is contained in the ring buffer
+     */
     bool contains(const T &value) const
     {
         return RBufBuffer.contains(value);
     }
 
+    /**
+     * \brief returns the location of a given value in the ring buffer
+     * 
+     * if the value is not contained in the ring buffer -1 is returned.
+     */
     int find(const T&value) const
     {
         int curIndex = RBufCurSize;
@@ -137,6 +188,12 @@ public:
         return -1;
     }
 
+    /**
+     * \brief returns the current size of the ring buffer
+     * 
+     * \note the size of the ring buffer never exceeds its set size, 
+     * however the current size of the ring buffer can be smaller.
+     */
     int size() const
     {
         return RBufCurSize;
@@ -147,6 +204,11 @@ public:
         return RBufBuffer.capacity();
     }
 
+    /**
+     * \brief clears the ring buffer and resets its location pointers
+     * 
+     * \warning This does not free the contained elements!
+     */
     void clear()
     {
         RBufBuffer = QVector<T>(RBufSize);
@@ -177,7 +239,17 @@ private:
 class MsgStorage
 {
 public:
+    /**
+     * \brief One message container
+     * 
+     * A MsgContainer is a simple vector that holds Msgs
+     */
     typedef QVector<Msg> MsgContainer;
+    /**
+     * \brief one conteiner to store message containers
+     * 
+     * A MsgContainerStore is a simple vector that holds MsgContainers
+     */
     typedef QVector<MsgContainer> MsgContainerStore;
 
     /**
@@ -208,18 +280,50 @@ public:
      */
     Msg at(const int index);
 
+    /**
+     * \brief appends a messages to the MsgStorage with a move operation
+     */
     void append(Msg &&value);
+    /**
+     * \brief appends a message to the MsgStorage with a copy operation
+     */
     void append( const Msg &value );
+    /**
+     * \brief replaces the message at the given index with the new message
+     */
     void replace( const int index, const Msg &value );
+    /**
+     * \brief remove the message at the given index
+     */
     void remove( const int index );
 
+    /**
+     * \brief clears the MsgStorage
+     * 
+     * \warning This does not free the contained elements!
+     */
     void clear();
 
+    /**
+     * \brief returns the current size of the MsgStorage
+     */
     int size() const;
+    /**
+     * \brief returns whether the MsgStorage is empty
+     */
     bool isEmpty() const;
+    /**
+     * \brief returns the current memory usage of the MsgStorage
+     */
     int MemUsage() const;
 
+    /**
+     * \brief parses the whole MsgStorage to a JSON document
+     */
     QJsonDocument parseToJson() const;
+    /**
+     * \brief reads in a MsgStorage from a JSON document
+     */
     bool parseFromJson(const QJsonArray &jsonMsgsArray);
 private:
     /**
@@ -244,12 +348,12 @@ private:
     void fetchContainerIDHelper(ContainerID &ContainerIDToFetch);
 
 private:
-    MsgContainerStore MsgStore;
-    MsgContainer LastContainer; //!< LastContaine is kept in memory permanently because of append method!
+    MsgContainerStore MsgStore; //!< The storage of the MsgContainers
+    MsgContainer LastContainer; //!< LastContainer is kept in memory permanently because of append method!
     QVector<QString> ContainerFileNames;
     const int ContainerSize;               //CONST BECAUSE THIS SHOULD NOT BE CHANGEABLE AT RUNTIME ONCE THE OBJECT WAS CREATED!
     const int NrOfContainersToKeepInRAM;        //CONST BECAUSE THIS SHOULD NOT BE CHANGEABLE AT RUNTIME ONCE THE OBJECT WAS CREATED!
-    RSimpleDestructiveRingBuff<ContainerID> ContainerInRAMIndexMapping;
+    RSimpleDestructiveRingBuff<ContainerID> ContainerInRAMIndexMapping; //!< Ring buffer that stores the mapping of indices that are kept in RAM (MsgStore)
     const QString FilePrefix;
     QDir MsgStorageTempDir;
     int CurrentSize;
