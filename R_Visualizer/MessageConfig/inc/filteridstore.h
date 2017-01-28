2d12604a414dbd0b6014868ddf79d9f2f7ba3dcc
Christian Neuberger <NeubergerCh500@th-nuernberg.de>
2017-01-28 16:54:03 +0100

    added doxygen commentary to filtercodestore and partly to filteridstore; WIP

--- R_Visualizer/MessageConfig/inc/filteridstore.h
+++ R_Visualizer/MessageConfig/inc/filteridstore.h
@@ -1,3 +1,10 @@
+/**
+ * \file filteridstore.h
+ * \author Christian Neuberger
+ * \date
+ * 
+ * \brief The Store that holds ids to filter for
+ */
 #ifndef FILTERIDSTORE_H
 #define FILTERIDSTORE_H
 
@@ -12,13 +19,36 @@ class FilterIDStore : public QAbstractListModel
 {
     Q_OBJECT
 public:
+    /**
+     * \brief constructs a FilterIDStore
+     * 
+     * \param[in] idModel Data Model that holds the IDs (for autocompletion and coloring)
+     */
     explicit FilterIDStore(IDModel &idModel, QObject *parent = 0);
 
+    /**
+     * \brief Returns the current row count/number of elements in the store
+     */
     int rowCount(const QModelIndex &parent) const Q_DECL_OVERRIDE;
+    /**
+     * \brief Returns the data of the given index corresponding to the given role
+     */
     QVariant data(const QModelIndex &index, int role) const Q_DECL_OVERRIDE;
+    /**
+     * \brief returns the data for the header (of the table)
+     */
     QVariant headerData(int section, Qt::Orientation orientation, int role) const Q_DECL_OVERRIDE;
+    /**
+     * \brief Sets the data for the given index to the given value corresponding to the given role
+     */
     bool setData(const QModelIndex &index, const QVariant &value, int role) Q_DECL_OVERRIDE;
+    /**
+     * \brief returns the ItemFlags for the given index
+     */
     Qt::ItemFlags flags(const QModelIndex &index) const Q_DECL_OVERRIDE;
+    /**
+     * \brief Remove count rows starting from row
+     */
     bool removeRows(int row, int count, const QModelIndex &parent = QModelIndex()) Q_DECL_OVERRIDE;
     void removeRow(int row, const QModelIndex &parent = QModelIndex() );
 
